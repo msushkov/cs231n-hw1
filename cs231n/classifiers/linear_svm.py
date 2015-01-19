bf26c00b01c2919e69c0796e2b958d48c15da676
Mikhail Sushkov <sushkovm@gmail.com>
2015-01-19 02:10:06 -0800

    vectorizing svm

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -29,13 +29,19 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[j, :] += X[:, i].T
+        dW[y[i], :] -= X[:, i].T
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
+
+  # Add regularization to the gradient
+  dW += reg * np.sum(W)
 
   #############################################################################
   # TODO:                                                                     #
@@ -45,7 +51,6 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
 
   return loss, dW
 
@@ -64,7 +69,33 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  
+  K = W.shape[0]
+  N = X.shape[1]
+  D = W.shape[1]
+  
+  # need a matrix of shape (N, D) where the rows are w_y0, w_y1, ..., w_yN-1
+
+  # need rows and cols for advanced indexing of W using elements in y as indices
+  cols = np.tile(np.arange(D), (N, 1))
+  y_col = np.reshape(y, (N, 1)) # make y a column vector
+  rows = np.tile(y_col, (1, D))
+
+  # [w_y0 * x0, w_y1 * x1, ..., w_yN-1 * xN-1]
+  wx = np.diag(np.dot(W[rows, cols], X))
+
+  result = np.dot(W, X) - wx + 1
+  result = np.maximum(np.zeros(N, D), result)
+
+  # sum over each column
+  # (subtract 1 since we overshot our sum by 1 for the j == y term that we didn't ignore)
+  result = np.sum(result, axis=0) - 1
+  
+  loss = np.sum(result) / float(N)
+
+  # Add regularization to the loss.
+  loss += 0.5 * reg * np.sum(W * W)
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
